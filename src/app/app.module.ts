80ad45bdc1625cf2d71a58c89af4e58460eced3c
Becket <becketvelasquez123@gmail.com>
2023-11-12 19:33:12 -0400

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
-import { EncabezadoComponent } from './pagina/encabezado/encabezado.component';
-import { CuerpoComponent } from './pagina/cuerpo/cuerpo.component';
-import { PiepaginaComponent } from './pagina/piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EncabezadoComponent,
-    CuerpoComponent,
-    PiepaginaComponent
   ],
   imports: [
     BrowserModule,
