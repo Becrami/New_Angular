3359134cedb963bc9e5e81e217376e460b295cc0
Becket <becketvelasquez123@gmail.com>
2023-11-21 23:17:38 -0400

    Pagina

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Data } from '@angular/router';
 import { LenguajesService } from 'src/app/services/lenguajes.service';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
@@ -11,9 +12,14 @@ export class HomeComponent implements OnInit {
 
   dataUsers: any = [];
   dataLanguajes: any = [];
-  data: any = [];
-  name: string = "Python";
-  abrev: string = "Py";
+  dataSource: any = [];
+  name: string = "";
+  abrev: string = "";
+  editingMode: boolean = false;
+  year: string = "";
+  creator: string = "";
+
+
 
   constructor(private usersServices: UsuariosService, private lenguajesServices: LenguajesService, private language: LenguajesService){}
 
@@ -22,25 +28,96 @@ export class HomeComponent implements OnInit {
     this.usersServices.getUsers().subscribe( (data) => {
       this.dataUsers = data;
     })
-    this.lenguajesServices.getListlanguages().subscribe( (data) => {
-      let arrayLenguajes = [data];
-      this.dataLanguajes = arrayLenguajes;
-    });
     this.language.getListlanguages().subscribe( (data )=> {
-      console.log(data)
+      for (var key in data) 
+      {
+        var row = {id:key, abrev: data[key].abrev, name: data[key].name,  year: data[key].year, creator: data[key].creator }
+        this.dataSource.push(row)
+      }
+      console.log(this.dataSource)
     })
   }
 
   save()
   {
-    var body =
+    let body =
     {
       name: this.name,
-      abrev: this.abrev
+      abrev: this.abrev,
+      year: this.year,
+      creator: this.creator
+
     }
     this.language.postlanguages(body).subscribe( (data) => {
-      console.log (data)
+      if(data!=null)
+      {
+        const newRow = {
+          id: data.id,
+          name: this.name,
+          abrev: this.abrev,
+          year: this.year,
+          creator: this.creator
+        };
+        this.dataSource.push(newRow);
+
+        this.name = '';
+        this.abrev = '';
+        this.year = '';
+        this.creator = '';
+
+        window.location.reload(); 
+      }
     })
   }
+  borrar(id:string){
+    let aux = confirm("Estas seguro de eliminar")
+    if(!aux) return
+    this.language.deletelanguages(id).subscribe( (data) => {
+      if(data==null)
+      {
+        window.location.reload(); 
+      }
+    })
+  }
+ 
+  editRow: { id: string, name: string, abrev: string, year: string, creator: string } = { id: '', name: '', abrev: '', year: '', creator: '' };
+  actualizar(id: string) {
+    let aux = confirm("Estás seguro de actualizar");
+    if (!aux) return;
+    // Encuentra la fila correspondiente en dataSource y almacena los datos en editRow
+    const foundRow = this.dataSource.find((row: { id: string, name: string, abrev: string, year: string, creator: string}) => row.id === id);
+
+    if (foundRow) {
+      this.editRow = { ...foundRow };
+  
+      // Asigna los valores a las variables name y abrev para que aparezcan en el formulario
+      this.name = this.editRow.name;
+      this.abrev = this.editRow.abrev;
+      this.year = this.editRow.year;
+      this.creator = this.editRow.creator
+  
+      this.editingMode = true; // Activa el modo de edición
+    } else {
+      console.error('No se encontró ninguna fila con el ID proporcionado.');
+    }
+  }
+  
+  update() {
+    // Actualiza los datos en dataSource usando los valores modificados
+    this.editRow.name = this.name;
+    this.editRow.abrev = this.abrev;
+    this.editRow.year = this.year;
+    this.editRow.creator = this.creator
+  
+    // Llama a tu servicio para actualizar los datos en la base de datos
+    this.language.updateLanguage(this.editRow.id, { name: this.name, abrev: this.abrev, country: this.year, creator: this.creator })
+      .subscribe((data) => {
+        if (data != null) {
+          this.editingMode = false; // Desactiva el modo de edición
+          window.location.reload();
+        }
+      });
+  }
+  
 
 }
--- src/app/services/lenguajes.service.ts
+++ src/app/services/lenguajes.service.ts
@@ -7,17 +7,32 @@ import { Observable } from 'rxjs';
 })
 export class LenguajesService {
 
-  url: string ="https://apirest-2fb0f-default-rtdb.firebaseio.com/v17topList.json"
+  url: string ="https://apirest-2fb0f-default-rtdb.firebaseio.com/v17topList";
+  apiurl: string = "https://paises-a8a38-default-rtdb.firebaseio.com/"
 
   constructor(private http: HttpClient) { }
 
   getListlanguages(): Observable<any> 
   {
-    return this.http.get(this.url)
+    let getUrl = this.url + ".json"
+    return this.http.get(getUrl)
   }
 
   postlanguages(body:any): Observable<any>
   {
-    return this.http.post(this.url, body) 
+    let postUrl = this.url + ".json"
+    return this.http.post(postUrl, body) 
+  }
+
+  deletelanguages(id:string): Observable<any>
+  {
+    let delUrl = this.url + "/" + id + ".json"
+    return this.http.delete(delUrl)
+  }
+
+  updateLanguage(id:string, body:any): Observable<any>
+  {
+    let uptUrl = this.url + "/" + id + ".json"
+    return this.http.put(uptUrl, body) 
   }
 }
